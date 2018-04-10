52f3a63f991f6075fcd698244f1234aab4a1bcce
codemass <worklette@yandex.ru>
2018-04-10 22:14:49 +0300

    Добавлен вспомогательный класс BeerExpert При выборе значения из спиннера, выводятся сорта пива

--- app/src/main/java/com/example/bbtt/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/example/bbtt/beeradviser/FindBeerActivity.java
@@ -2,12 +2,40 @@ package com.example.bbtt.beeradviser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.List;
 
 public class FindBeerActivity extends AppCompatActivity {
+    private BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
     }
+
+    //Вызывается при щелчке на кнопке
+
+    public void onClickFindBeer(View view) {
+        //Получить ссылку на TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        //Подучить ссылку на Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        //Получить вариант выбранный в Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        //Получить рекомендации от класса BeerExpert
+        List<String> brandList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+        //Вывести сорта пива
+        brands.setText(brandsFormatted);
+    }
+
 }
